eae716103fe81af156c58064e90cc8b56f0d779c
Husayn Kartodirdjo <hkdirdjo@gmail.com>
2024-10-23 23:17:30 -0400

    Fixed many errors

--- BLAforKalman.h
+++ BLAforKalman.h
@@ -11,48 +11,60 @@ private:
     /* data */
     BLA::Matrix<NUM_STATES,NUM_STATES,double> f, q, p;
     BLA::Matrix<NUM_STATES,NUM_COM,double> b;
-    BLA::Matrix< NUM_OBS, NUM_STATES, double > h
-    BLA::Matrix<NUM_OBS, NUM_OBS, double > r
     BLA::Matrix<NUM_STATES, 1, double > x;
     BLA::Matrix<NUM_OBS_GPS, 1, double > zGPS; // posE, posN, posD
     BLA::Matrix<NUM_OBS_BARO,1, double > zBaro; // posD
+
+    BLA::Matrix<NUM_STATES,NUM_STATES> createIdentityMatrix() {
+        BLA::Matrix<NUM_STATES,NUM_STATES> I = BLA::Zeros<NUM_STATES,NUM_STATES>();
+        for (int i = 0; i < NUM_STATES; i++)
+        {
+            I(i,i) = 1;
+        }
+        return I;
+    }
+
 public:
     KalmanFilter(BLA::Matrix<NUM_STATES,NUM_STATES,double>& f, //Arguments
                  BLA::Matrix<NUM_STATES,NUM_STATES,double>& q,
                  BLA::Matrix<NUM_STATES,NUM_STATES,double>& p,
-                 BLA::Matrix<NUM_STATES,NUM_COM,double>& b,
+                 BLA::Matrix<NUM_STATES,NUM_COM,double>& b
                 ):
                 f(f), // Initializer list
                 q(q),
                 p(p),
-                b(b),
-                h(h),
-                r(r)
+                b(b)
                 {
                     // Constructor; allocate resources
                     x = BLA::Zeros<NUM_STATES,1>();
                 }
+    void predict(double dt, const BLA::Matrix<NUM_COM,1,double>& u);
+    void updateGPS(const BLA::Matrix<NUM_OBS_GPS,1,double>& zGPS, float HDOP, float VDOP);
+    void updateBarometer(double zBaro);
+    BLA::Matrix<NUM_STATES,1> state() const;
 };
 
-void predict(double dt, const BLA::Matrix<NUM_COM,1,double>& u){
+template <int NUM_STATES, int NUM_OBS_GPS, int NUM_OBS_BARO, int NUM_COM>
+void KalmanFilter<NUM_STATES, NUM_OBS_GPS, NUM_OBS_BARO, NUM_COM>::predict(double dt, const BLA::Matrix<NUM_COM,1,double>& u){
     // update F matrix
-    f(0,3) = deltaTime;
-    f(1,4) = deltaTime;
-    f(2,5) = deltaTime;
+    this->f(0,3) = dt;
+    this->f(1,4) = dt;
+    this->f(2,5) = dt;
 
     // update B matrix
-    b(0,0) = 0.5*pow(deltaTime,2);
-    b(1,1) = b(0,0);
-    b(2,2) = b(0,0);
-    b(3,0) = deltaTime;
-    b(4,1) = deltaTime;
-    b(5,2) = deltaTime;
+    this->b(0,0) = 0.5*pow(dt,2);
+    this->b(1,1) = b(0,0);
+    this->b(2,2) = b(0,0);
+    this->b(3,0) = dt;
+    this->b(4,1) = dt;
+    this->b(5,2) = dt;
 
-    x = f*x + b*u;
-    p = f*p*~f + q;
+    this->x = this->f * this->x + this->b * u;
+    this->p = this->f * this->p * ~this->f + this->q;
 }
 
-void updateGPS(const BLA::Matrix<NUM_OBS_GPS,1,double>& zGPS, float HDOP, float VDOP){
+template <int NUM_STATES, int NUM_OBS_GPS, int NUM_OBS_BARO, int NUM_COM>
+void KalmanFilter<NUM_STATES, NUM_OBS_GPS, NUM_OBS_BARO, NUM_COM>::updateGPS(const BLA::Matrix<NUM_OBS_GPS,1,double>& zGPS, float HDOP, float VDOP){
     BLA::Matrix< NUM_OBS_GPS, NUM_STATES, double > h = {1.0, 0.0, 0.0, 0.0, 0.0, 0.0,
                                                         0.0, 1.0, 0.0, 0.0, 0.0, 0.0,
                                                         0.0, 0.0, 1.0, 0.0, 0.0, 0.0};
@@ -63,14 +75,15 @@ void updateGPS(const BLA::Matrix<NUM_OBS_GPS,1,double>& zGPS, float HDOP, float 
     BLA::Matrix<NUM_OBS_GPS, NUM_OBS_GPS, double > temp;
     BLA::Matrix<NUM_STATES, NUM_STATES, double > temp2;
 
-    temp = h*p*~h + r;
-    k = p*~h*Inverse(temp);
-    x += k*(zGPS - h*x);
-    temp2 = BLA::Identity<NUM_STATES,NUM_STATES>() - k*h;
-    p = temp2*p*~temp2 + k*r*~k;
+    temp = h * this->p * ~h + r;
+    k = this->p * ~h * Inverse(temp);
+    this->x += k * (zBaro - h * this->x);
+    temp2 = this->createIdentityMatrix() - k * h;
+    this->p = temp2 * this->p * ~temp2 + k * r * ~k;
 }
 
-void updateBarometer(double zBaro) {
+template <int NUM_STATES, int NUM_OBS_GPS, int NUM_OBS_BARO, int NUM_COM>
+void KalmanFilter<NUM_STATES, NUM_OBS_GPS, NUM_OBS_BARO, NUM_COM>::updateBarometer(double zBaro) {
     BLA::Matrix<NUM_OBS_BARO, NUM_STATES, double > h = {0.0, 0.0, 1.0, 0.0, 0.0, 0.0};
     BLA::Matrix<NUM_OBS_BARO, NUM_OBS_BARO, double > r = {1.0};
     BLA::Matrix<NUM_STATES, NUM_OBS_BARO, double > k;
@@ -80,12 +93,13 @@ void updateBarometer(double zBaro) {
     temp = h*p*~h + r;
     k = p*~h*Inverse(temp);
     x += k*(zBaro - h*x);
-    temp2 = BLA::Identity<NUM_STATES,NUM_STATES>() - k*h;
+    temp2 = this->createIdentityMatrix() - k*h;
     p = temp2*p*~temp2 + k*r*~k;
 }
 
-BLA::Matrix<NUM_STATES,1> state() const {
-    return x;
+template <int NUM_STATES, int NUM_OBS_GPS, int NUM_OBS_BARO, int NUM_COM>
+BLA::Matrix<NUM_STATES,1> KalmanFilter<NUM_STATES, NUM_OBS_GPS, NUM_OBS_BARO, NUM_COM>::state() const {
+    return this->x;
 }
 
 #endif
